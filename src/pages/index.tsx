b87046635a6215dc056b5c01a012682e709411c8
JuliaJPereira <juliapereira.odc@gmail.com>
2024-03-10 20:51:33 -0300

    Desenvolver listagem de produtos com ISR - Incremental Static Regeneration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,54 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import ProductList from "./listagem";
+import { ProductItem, ProductListContainer } from '@/components/Listagem'
+import useSWR from 'swr';
 
-const inter = Inter({ subsets: ["latin"] });
+// export default function ProductList() {
+//     const { data, error } = useSWR('https://fakestoreapi.com/products',
+//         (...arg) => fetch(...arg)
+//             .then(res => res.json()))
 
-export default function Home() {
-  return (
-    <>
-      <ProductList />
-    </>
-  );
+//     if (error) {
+//         return <div>Erro: Deu ruim</div>
+//     }
+
+//     if (!data) {
+//         return <div>Carregando...</div>
+//     }
+
+//     return (
+//         <div>
+//             {data.map((product: any) => (
+//                 <ProductListContainer id='product-list-container'>
+//                     <ProductItem id='product-item'>{product.title}</ProductItem>
+//                 </ProductListContainer>
+//             ))}
+//         </div>
+//     )
+// }
+
+export async function getStaticProps() {
+  const products = await fetch('https://fakestoreapi.com/products'
+  ).then((res) => res.json());
+  const date = new Date();
+  return {
+    props: {
+      products,
+      lastRender: date.getSeconds(),
+    },
+    revalidate: 5,
+  }
 }
+
+const ProductList = (props: any) => {
+  return (
+    <div>
+      <div>{props.lastRender}</div>
+      {props.products.map((product: any) => (
+        <ProductListContainer id='product-list-container'>
+          <ProductItem id='product-item'>{product.title}</ProductItem>
+        </ProductListContainer>
+      ))}
+    </div>
+  )
+}
+
+export default ProductList;
