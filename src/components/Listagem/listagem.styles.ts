07421db8c343217a2837c37caa7ba70bdd383fda
JuliaJPereira <juliapereira.odc@gmail.com>
2024-03-10 20:35:32 -0300

    commit inicial

---
+++ src/components/Listagem/listagem.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ProductListContainer = styled.div`
+  display: inline-block;
+  // flex-wrap: wrap;
+`;
+
+export const ProductItem = styled.div`
+  width: 200px;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  height: 100px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
