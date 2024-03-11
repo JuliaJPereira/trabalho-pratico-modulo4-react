850f25c7aea58431fa67893fcb0b01735d96eba4
JuliaJPereira <juliapereira.odc@gmail.com>
2024-03-10 21:01:55 -0300

    Remover código comentado e adicionar tag Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,5 @@
 import { ProductItem, ProductListContainer } from '@/components/Listagem'
-import useSWR from 'swr';
-
-// export default function ProductList() {
-//     const { data, error } = useSWR('https://fakestoreapi.com/products',
-//         (...arg) => fetch(...arg)
-//             .then(res => res.json()))
-
-//     if (error) {
-//         return <div>Erro: Deu ruim</div>
-//     }
-
-//     if (!data) {
-//         return <div>Carregando...</div>
-//     }
-
-//     return (
-//         <div>
-//             {data.map((product: any) => (
-//                 <ProductListContainer id='product-list-container'>
-//                     <ProductItem id='product-item'>{product.title}</ProductItem>
-//                 </ProductListContainer>
-//             ))}
-//         </div>
-//     )
-// }
+import Link from 'next/link';
 
 export async function getStaticProps() {
   const products = await fetch('https://fakestoreapi.com/products'
@@ -44,7 +20,11 @@ const ProductList = (props: any) => {
       <div>{props.lastRender}</div>
       {props.products.map((product: any) => (
         <ProductListContainer id='product-list-container'>
-          <ProductItem id='product-item'>{product.title}</ProductItem>
+          <ProductItem id='product-item'>
+            <Link href={`/product/${product.id}`} style={{ textDecoration: 'none', color: 'black' }} >
+              {product.title}
+            </Link>
+          </ProductItem>
         </ProductListContainer>
       ))}
     </div>
