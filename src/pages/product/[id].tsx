65e866f821cb188d94670ec97187f6f4e10d8985
JuliaJPereira <juliapereira.odc@gmail.com>
2024-03-10 21:41:08 -0300

    Ajustes página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,27 +1,5 @@
 import useSWR from 'swr';
 
-function ProductPrice() {
-    const { data, error } = useSWR('https://fakestoreapi.com/products',
-        (...arg) => fetch(...arg)
-            .then(res => res.json()))
-
-    if (error) {
-        return <div>Erro: Deu ruim</div>
-    }
-
-    if (!data) {
-        return <div>Carregando...</div>
-    }
-
-    return (
-        <div>
-            {data.map((product: any) => (
-                <div>{product.price}</div>
-            ))}
-        </div>
-    )
-}
-
 export async function getStaticPaths() {
 
     const product = await fetch(
@@ -30,7 +8,7 @@ export async function getStaticPaths() {
 
     const paths = product.map((item: any) => ({
         params: {
-            id: item.id
+            id: String(item.id)
         }
     }));
 
@@ -52,10 +30,17 @@ export async function getStaticProps({ params }: any) {
 
 const Static = ({ product }: { product: any }): any => {
 
+    const { data } = useSWR('https://fakestoreapi.com/products',
+        (...arg) => fetch(...arg)
+            .then(res => res.json()));
+
     return (
         <div>
-            <ProductPrice />
-            <div>{product[0].name}</div>
+            <div>{product.title}</div>
+            <div>{product.description}</div>
+            <div>{product.category}</div>
+            <div>{data?.find((item: any) => item.id === product.id).price}</div>
+            <img src={product.image} alt={product.title} style={{ width: '100px' }} />
         </div>
     )
 }
